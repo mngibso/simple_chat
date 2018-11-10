52d6856840ea350054d90e9cd93926e1daa4cdb0
mngibson <44854368+mngibson@users.noreply.github.com>
2018-11-09 22:16:45 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import "github.com/gorilla/mux"
+
+func main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", HomeHandler)
+    r.HandleFunc("/products", ProductsHandler)
+    r.HandleFunc("/articles", ArticlesHandler)
+    http.Handle("/", r)
+}
